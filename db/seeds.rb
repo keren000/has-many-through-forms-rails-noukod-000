409e3068d3b09b1152eb2430901e6a12b8400b84
Keren-Happuc Fleurimond <keren@noukod.com>
2019-01-29 17:51:04 +0000

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+city = Category.create name: 'city'
+state = Category.create name: 'state'
