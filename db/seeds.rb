df8751afccba5671df7ad4f371207cbdfb4ef6b8
Keren-Happuc Fleurimond <keren@noukod.com>
2019-01-29 17:47:17 +0000

    Automatically backed up by Learn

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+music = Category.create name: 'music'
+news = Category.create name: 'news'
